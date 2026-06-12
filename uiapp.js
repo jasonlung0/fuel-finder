3b9d11618365d46c3f87101d01110ab605e0e3ad
jasonlung0 <jasonlung0@gmail.com>
2026-06-12 23:45:23 +0100

    new code 2

--- uiapp.js
+++ uiapp.js
@@ -2,7 +2,6 @@
 const TOMTOM_API_KEY = 'JY2i0gGmgtYakfiO1T3XOobPhgkGpFC6';
 const OCM_KEY = 'e1b259fb-c770-45f8-9e4d-069a19631b2e';
 const OPENWEATHER_API_KEY = '5e67010087dac92dd2eb31bc4c0a2abf'; 
-const weatherCacheMap = new Map();
 const forecourtTrafficCacheMap = new Map();
 
 // --- UNIVERSAL ID HELPER ---
